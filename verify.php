4a5d4ac65139daaf13d910028797c5b57d888c85
Sayumpoo Chansuta <sayumpoo.c@jasmine.com>
2018-02-28 11:42:30 +0700

    Edit bot.php

--- verify.php
+++ verify.php
@@ -1,5 +1,6 @@
 <?php
 $access_token = 'OBndRkQEmWPqffF0HTvXjhbX0W5XUt2cqYfTv46lbblXx0+H5H1oaJseLWjmGPeLujCTXLjxk6+kEplq9FVZCEykT57s6OW+59fKQo9929dS0VbPzM3u5QH152w9+irinIPDtCYTCZL7yd8WsdDLdAdB04t89/1O/w1cDnyilFU=';
+
 $url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
