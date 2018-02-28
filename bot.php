772a21f71c417f29e28ffee5eaa6531c98a14c23
Sayumpoo Chansuta <sayumpoo.c@jasmine.com>
2018-02-28 16:43:38 +0700

    1

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 // include composer autoload
-require_once '../vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
 $access_token = 'OBndRkQEmWPqffF0HTvXjhbX0W5XUt2cqYfTv46lbblXx0+H5H1oaJseLWjmGPeLujCTXLjxk6+kEplq9FVZCEykT57s6OW+59fKQo9929dS0VbPzM3u5QH152w9+irinIPDtCYTCZL7yd8WsdDLdAdB04t89/1O/w1cDnyilFU=';
 
