1629ad43c407483bdf2082f8e48bcb742f57139e
Sayumpoo Chansuta <sayumpoo.c@jasmine.com>
2018-02-28 16:48:20 +0700

    1

--- bot.php
+++ bot.php
@@ -79,11 +79,9 @@ if (!is_null($events['events'])) {
                       ];
   }else if($text=='เฌอปราง'){
 
-                      $placeName = "ที่ตั้งร้าน";
-                    $placeAddress = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
-                    $latitude = 13.780401863217657;
-                    $longitude = 100.61141967773438;
-                    $messages = new LocationMessageBuilder($placeName, $placeAddress, $latitude ,$longitude);
+    $messages =       [		'type' => 'text',
+                          'text' => 'คิดถึงนะจ๊ะตั้งใจทำงานหละ'
+                      ];
   }else{
 
     $messages =       [		'type' => 'text',
