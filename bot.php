5deae13357bf812c52991c654ba87cc65516b88e
metrocom-yo <sayumpooo_yo@hotmail.com>
2018-02-28 23:03:27 +0700

    edit bot.php

--- bot.php
+++ bot.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// include composer autoload
+// include composer autoload1
 require_once 'vendor/autoload.php';
 
 // การตั้งเกี่ยวกับ bot
