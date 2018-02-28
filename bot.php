581651dc7878355b780b2909c6b038a512a5998c
Sayumpoo Chansuta <sayumpoo.c@jasmine.com>
2018-02-28 13:35:59 +0700

    Edit bot.php

--- bot.php
+++ bot.php
@@ -10,17 +10,23 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
   // Loop through each event
   foreach ($events['events'] as $event) {
+
   // Reply only when message sent is in 'text' format
   if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+
     // Get text sent
     $text = $event['message']['text'];
+
     // Get replyToken
     $replyToken = $event['replyToken'];
+
     // Build message to reply back
     $messages = [				'type' => 'text',				'text' => $text			];
+    
     // Make a POST Request to Messaging API to reply to sender
     $url = 'https://api.line.me/v2/bot/message/reply';
-    $data = [				'replyToken' => $replyToken,				'messages' => [$messages],			];
+    $data = [				'replyToken' => $replyToken,
+    'messages' => [$messages],			];
     $post = json_encode($data);
     $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
     $ch = curl_init($url);
